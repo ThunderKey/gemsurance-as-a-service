12816e50fb87140cde6560ff48da9bbc658275cc
Nicolas Ganz <nicolas@keltec.ch>
2017-10-23 09:35:55 +0200

    ignore unauthenticated message

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,9 +1,15 @@
 class Users::SessionsController < Devise::SessionsController
   def new
-    if flash.empty? && Devise.omniauth_providers.count == 1
+    if known_flash? && Devise.omniauth_providers.count == 1
       redirect_to omniauth_authorize_path(:user, Devise.omniauth_providers.first)
     else
       super
     end
+  end
+
+  private
+
+  def known_flash?
+    flash.empty? || flash.to_hash == {'alert' => t('devise.failure.unauthenticated')}
   end
 end
