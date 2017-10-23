9e0dbcaf7886f4da694a557058dfedca18e022bc
Nicolas Ganz <nicolas@keltec.ch>
2017-10-23 16:48:25 +0200

    fixed re-login loop if you sign out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,10 @@ class ApplicationController < ActionController::Base
     new_user_session_path
   end
   # :nocov:
+
+  private
+
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path
+  end
 end
